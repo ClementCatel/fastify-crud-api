bfac7b0139bdd5a32a9f58e152e9ec5754170ce6
Clement Catel <clement.catel4@gmail.com>
2025-03-16 01:09:20 +0100

    fix register

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,89 +1,122 @@
-import { FastifyInstance } from "fastify";
-import bcrypt from "bcrypt";
-import { prisma } from "../lib/prisma";
-import jwt from "jsonwebtoken";
+import { FastifyInstance } from 'fastify'
+import bcrypt from 'bcrypt'
+import { prisma } from '../lib/prisma'
+import jwt from 'jsonwebtoken'
 
-const JWT_SECRET = process.env.JWT_SECRET as string;
+const JWT_SECRET = process.env.JWT_SECRET as string
 
 interface RegisterBody {
-  email: string;
-  username: string;
-  password: string;
+	email: string
+	username: string
+	password: string
 }
 
 interface LoginBody {
-  email: string;
-  password: string;
+	email: string
+	password: string
 }
 
 export async function auth(app: FastifyInstance) {
-  app.post("/register", async (request, reply) => {
-    const { email, username, password } = request.body as RegisterBody;
+	app.post('/register', async (request, reply) => {
+		const { email, username, password } = request.body as RegisterBody
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+		const hashedPassword = await bcrypt.hash(password, 10)
 
-    try {
-      const user = await prisma.user.create({
-        data: {
-          email,
-          avatarUrl: `https://api.dicebear.com/9.x/initials/svg?seed=${username}`,
-          username,
-          password: hashedPassword,
-        },
-      });
+		try {
+			const user = await prisma.user.create({
+				data: {
+					email,
+					avatarUrl: `https://api.dicebear.com/9.x/initials/svg?seed=${username}`,
+					username,
+					password: hashedPassword,
+				},
+			})
 
-      reply.code(201).send({id: user.id, email: user.email, username: user.username});
-    } catch (error) {
-      reply.code(400).send({ message: "User already exists" });
-    }
-  });
+			const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+				expiresIn: '7d',
+			})
 
-  app.post("/login", async (request, reply) => {
-    const { email, password } = request.body as LoginBody;
-    console.log(email);
-    const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
+			reply
+				.code(201)
+				.send({
+					user: {
+						id: user.id,
+						email: user.email,
+						username: user.username,
+						avatarUrl: user.avatarUrl,
+					},
+					token,
+				})
+		} catch (error) {
+			reply.code(400).send({ message: 'User already exists' })
+		}
+	})
 
-    if (!user) {
-      return reply.code(401).send({ message: "Invalid email or password 1" });
-    }
+	app.post('/login', async (request, reply) => {
+		const { email, password } = request.body as LoginBody
+		console.log(email)
+		const user = await prisma.user.findUnique({
+			where: {
+				email,
+			},
+		})
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
+		if (!user) {
+			return reply
+				.code(401)
+				.send({ message: 'Invalid email or password' })
+		}
 
-    if (!passwordMatch) {
-      return reply.code(401).send({ message: "Invalid email or password 2" });
-    }
+		const passwordMatch = await bcrypt.compare(password, user.password)
 
-    const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "7d" });
-    reply.send({ token });
-  }); 
+		if (!passwordMatch) {
+			return reply
+				.code(401)
+				.send({ message: 'Invalid email or password' })
+		}
 
-  app.get("/me", async (request, reply) => {
-    try {
-      const authHeader = request.headers.authorization;
-      if (!authHeader) {
-        return reply.code(401).send({ message: "Unauthorized" });
-      }
+		const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+			expiresIn: '7d',
+		})
+		reply.send({
+			user: {
+				id: user.id,
+				email: user.email,
+				username: user.username,
+				avatarUrl: user.avatarUrl,
+			},
+			token,
+		})
+	})
 
-      const token = authHeader.replace("Bearer ", "");
-      const payload = jwt.verify(token, JWT_SECRET) as { userId: string };
+	app.get('/me', async (request, reply) => {
+		try {
+			const authHeader = request.headers.authorization
+			if (!authHeader) {
+				return reply.code(401).send({ message: 'Unauthorized' })
+			}
 
-      const user = await prisma.user.findUnique({
-        where: {
-          id: payload.userId,
-        },
-      });
+			const token = authHeader.replace('Bearer ', '')
+			const payload = jwt.verify(token, JWT_SECRET) as { userId: string }
 
-      if (!user) {
-        return reply.code(401).send({ message: "User not found" });
-      }
+			const user = await prisma.user.findUnique({
+				where: {
+					id: payload.userId,
+				},
+			})
 
-      reply.send({ id: user.id, email: user.email, username: user.username });
-    } catch (error) {
-      reply.code(401).send({ message: "Invalid token" });
-    }
-  });
+			if (!user) {
+				return reply.code(401).send({ message: 'User not found' })
+			}
+
+			reply.send({
+				id: user.id,
+				email: user.email,
+				username: user.username,
+				avatarUrl: user.avatarUrl,
+			})
+		} catch (error) {
+			reply.code(401).send({ message: 'Invalid token' })
+		}
+	})
 }
